e96a88fa0aa6fce9131f7174e45cb2b291233609
Vaithilingam97 <vaithilingam97@gmail.com>
2023-05-07 03:07:04 +0530

    FirstWebCode

---
+++ script.js
@@ -0,0 +1,45 @@
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", async (event) => {
+
+  event.preventDefault();
+  const searchInput = document.getElementById("searchInput").value;
+  
+  try {
+    const response = await fetch(`https://api.openbrewerydb.org/breweries/search?query=${searchInput}`);
+    const data = await response.json();
+    const table = document.getElementById("resultTable");
+
+    table.innerHTML = "";
+
+    const headerRow = table.insertRow();
+    const nameHeader = headerRow.insertCell(0);
+    const typeHeader = headerRow.insertCell(1);
+    const addressHeader = headerRow.insertCell(2);
+    const websiteHeader = headerRow.insertCell(3);
+    const phoneHeader = headerRow.insertCell(4);
+    nameHeader.innerHTML = "<b>Name</b>";
+    typeHeader.innerHTML = "<b>Type</b>";
+    addressHeader.innerHTML = "<b>Address</b>";
+    websiteHeader.innerHTML = "<b>Website</b>";
+    phoneHeader.innerHTML = "<b>Phone</b>";
+
+    data.forEach((brewery) => {
+      const row = table.insertRow();
+      const nameCell = row.insertCell(0);
+      const typeCell = row.insertCell(1);
+      const addressCell = row.insertCell(2);
+      const websiteCell = row.insertCell(3);
+      const phoneCell = row.insertCell(4);
+      nameCell.innerText = brewery.name;
+      typeCell.innerText = brewery.brewery_type;
+      addressCell.innerText = `${brewery.street}, ${brewery.city}, ${brewery.state}, ${brewery.postal_code}`;
+      websiteCell.innerHTML = `<a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>`;
+      phoneCell.innerText = brewery.phone || "-";
+    });
+  } catch (error) {
+
+    console.error(error);
+    alert("Failed to fetch data from Open Brewery API.");
+  }
+});
